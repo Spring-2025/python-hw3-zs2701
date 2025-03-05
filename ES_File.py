a097014ba9bd812de09b531ecaff5b3107115d2d
zs2701 <zs2701@columbia.edu>
2025-03-05 15:57:40 -0500

    Update ES_File.py

--- ES_File.py
+++ ES_File.py
@@ -7,11 +7,3 @@ def ES(losses,alpha=None,VaR=None,use_PnL=False):
   es_value=np.mean(losses[losses>VaR]) if np.any(losses>VaR) else VaR
   return es_value
 
-np.random.seed(40)
-u = np.random.uniform(0, 100, 100000)
-
-es_confidence = ES(losses=u, alpha=0.8)
-print('ES with confidence:', np.round(es_confidence, 0) == 90)
-
-es_var = ES(losses=u, VaR=80)
-print('ES with VaR:', np.round(es_var, 0) == 90)
