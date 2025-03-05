f94ff66d4422b556cf6abb5fad642a625d1220f1
zs2701 <zs2701@columbia.edu>
2025-03-05 15:56:22 -0500

    Update ES_File.py

--- ES_File.py
+++ ES_File.py
@@ -1,16 +1,16 @@
 #ES
 import numpy as np
 
-def ES(losses,confidence=None,VaR=None,use_PnL=False):
+def ES(losses,alpha=None,VaR=None,use_PnL=False):
   if VaR is None:
-    VaR=np.percentile(losses,100*confidence)
+    VaR=np.percentile(losses,100*alpha)
   es_value=np.mean(losses[losses>VaR]) if np.any(losses>VaR) else VaR
   return es_value
 
 np.random.seed(40)
 u = np.random.uniform(0, 100, 100000)
 
-es_confidence = ES(losses=u, confidence=0.8)
+es_confidence = ES(losses=u, alpha=0.8)
 print('ES with confidence:', np.round(es_confidence, 0) == 90)
 
 es_var = ES(losses=u, VaR=80)
