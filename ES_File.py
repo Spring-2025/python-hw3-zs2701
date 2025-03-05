5b66e79cf180f493ff875448723f8f9ac1fa4aa3
zs2701 <zs2701@columbia.edu>
2025-03-05 16:02:31 -0500

    Update ES_File.py

--- ES_File.py
+++ ES_File.py
@@ -4,6 +4,6 @@ import numpy as np
 def ES(losses,alpha=None,VaR=None,use_PnL=False):
   if VaR is None:
     VaR=np.percentile(losses,100*(1-alpha))
-  es_value=np.mean(losses[losses>VaR]) if np.any(losses>VaR) else VaR
+  es_value=np.mean(losses[losses=<VaR]) if np.any(losses=<VaR) else VaR
   return es_value
 
